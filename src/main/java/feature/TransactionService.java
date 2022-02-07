c1fd74f3f028f9766b05aa3a0fe603a80732b7b9
oawood93 <oa.wood@outlook.com>
2022-02-07 17:16:47 +0000

    green

--- src/main/java/feature/TransactionService.java
+++ src/main/java/feature/TransactionService.java
@@ -3,11 +3,11 @@ package feature;
 public class TransactionService {
 
     public Integer deposit(Integer amount, Integer balance) {
-        return amount + balance;
+        return balance + amount;
     }
 
     public Integer withdraw(Integer amount, Integer balance) {
-        return 0;
+        return balance - amount;
     }
 
 
