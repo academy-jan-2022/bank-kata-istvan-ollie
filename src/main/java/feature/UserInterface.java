60e19ec0aa6087e01b5b9141d09e8ef486b343fd
iagardi <garbigz@gmail.com>
2022-02-07 15:52:21 +0000

    red

---
+++ src/main/java/feature/UserInterface.java
@@ -0,0 +1,7 @@
+package feature;
+
+public class UserInterface {
+	public void printline(String s) {
+
+	}
+}
--- src/test/java/feature/AcceptanceTests.java
+++ src/test/java/feature/AcceptanceTests.java
@@ -1,8 +1,17 @@
 package feature;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
 public class AcceptanceTests {
+
+    @Mock UserInterface mockUI;
+
 
     @Test
     void account_prints_balance(){
@@ -11,6 +20,13 @@ public class AcceptanceTests {
         account.withdraw(100);
         account.deposit(200);
         account.printStatement();
+
+        verify(mockUI).printline(
+                "Date || Amount || Balance\n" +
+                "14/01/2012 || -500 || 2500\n" +
+                "13/01/2012 || 2000 || 3000\n" +
+                "10/01/2012 || 1000 || 1000"
+        );
 
     }
 }
