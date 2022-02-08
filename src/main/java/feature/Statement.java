0524b74a6e5d5726d85116405d252d5cfeff83ef
iagardi <garbigz@gmail.com>
2022-02-08 13:53:33 +0000

    green

--- src/main/java/feature/Statement.java
+++ src/main/java/feature/Statement.java
@@ -1,17 +1,18 @@
 package feature;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Statement {
-
+    private final ArrayList<Transaction> transactions = new ArrayList<Transaction>();
 
 
     public void add(Transaction transaction) {
-
+        transactions.add(transaction);
     }
 
     public List<Transaction> getTransactions() {
-        return new ArrayList<Transaction>(List.of());
+        return Collections.unmodifiableList(transactions);
     }
 }
