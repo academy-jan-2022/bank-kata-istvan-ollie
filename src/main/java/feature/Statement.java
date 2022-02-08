cd3267e744c9e0e703ca85536e753507f30c400b
iagardi <garbigz@gmail.com>
2022-02-08 13:40:22 +0000

    green

--- src/main/java/feature/Statement.java
+++ src/main/java/feature/Statement.java
@@ -1,14 +1,17 @@
 package feature;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Statement {
+
+
 
     public void add(Transaction transaction) {
 
     }
 
     public List<Transaction> getTransactions() {
-        return null;
+        return new ArrayList<Transaction>(List.of());
     }
 }
