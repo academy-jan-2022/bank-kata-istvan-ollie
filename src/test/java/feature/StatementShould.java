a568cee919418f2884acb41fccd85c4726b43ac0
iagardi <garbigz@gmail.com>
2022-02-08 13:50:11 +0000

    red

--- src/test/java/feature/StatementShould.java
+++ src/test/java/feature/StatementShould.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class StatementShould {
@@ -13,6 +14,17 @@ public class StatementShould {
 		var statement = new Statement();
 		var result = statement.getTransactions();
 		var expected = new ArrayList<Transaction>(List.of());
+
+		Assertions.assertEquals(expected, result);
+	}
+
+	@Test
+	void returns_a_list_of_1_when_recording_1_transaction(){
+		var statement = new Statement();
+		var transaction = new Transaction(100, new Date());
+		var expected = new ArrayList<Transaction>(List.of(transaction));
+		statement.add(transaction);
+		var result = statement.getTransactions();
 
 		Assertions.assertEquals(expected, result);
 	}
