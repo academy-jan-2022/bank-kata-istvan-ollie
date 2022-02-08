60141c23541d93818679569b4c928c0b4d93f64a
oawood93 <oa.wood@outlook.com>
2022-02-08 16:06:34 +0000

    broken test fixed

--- src/test/java/feature/DateProviderShould.java
+++ src/test/java/feature/DateProviderShould.java
@@ -12,17 +12,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DateProviderShould {
 
     @Test
-    void return_a_date_object(){
-        DateProvider dateProvider = new SimpleDateProvider();
-        var result = dateProvider.UtcNow();
-        assertThat(result, instanceOf(Date.class));
-    }
-
-    @Test
     void return_a_date_object_in_dd_mm_yyyy_format() {
         DateProvider dateProvider = new SimpleDateProvider();
         var result = dateProvider.UtcNow();
-        var expected = new SimpleDateFormat("dd/mm/yyyy").format(new Date());
+        var expected = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
 
         assertEquals(expected, result);
     }
